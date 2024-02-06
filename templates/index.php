1756b816a265272a4a1dd3326e4b099872dd210a
shihab4513 <shihab4513@gmail.com>
2024-02-06 20:11:52 +0600

    control flow alt syntax, here we have seen for loop and if statment '{}' alternate

--- templates/index.php
+++ templates/index.php
@@ -41,16 +41,16 @@ $i=0;
 <h4 class="center grey-text">Pizzas</h4>
 <div class="container">
     <div class="row">
-        <?php foreach ($pzzas as $pizza) {?>
+        <?php foreach ($pzzas as $pizza) :?>
         <div class="col s6  md3">
             <div class="card z-depth-0">
                 <div class="card-content center">
                     <h6><?php echo htmlspecialchars($pizza['title'])?></h6>
 
                     <ul>
-                        <?php foreach (explode(',',$pzzas[$i]['ingredients']) as $ing){ ?>
+                        <?php foreach (explode(',',$pzzas[$i]['ingredients']) as $ing): ?>
                         <li><?php echo htmlspecialchars($ing); ?></li>
-                        <?php  } $i=$i+1; ?>
+                        <?php  endforeach; $i=$i+1; ?>
                     </ul>
                     <div><?php echo htmlspecialchars($pizza['email'])?></div>
                 </div>
@@ -60,7 +60,12 @@ $i=0;
             </div>
 
         </div>
-        <?php } ?>
+        <?php endforeach; ?>
+        <?php if (count($pzzas)>=3): ?>
+        <p>there are 3 or more pizzas</p>
+        <?php else: ?>
+        <p>there are less than 3 pizzas</p>
+        <?php endif; ?>
     </div>
 </div>
 
@@ -68,5 +73,5 @@ $i=0;
 
 </body>
 </html>
-
+<!--we can use ':' for first bracket 'endforeach' for second bracket -->
 
